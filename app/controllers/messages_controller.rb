6623ea0629ed9fb1928eda3cfafc623d3a745fc3
野本佳裕 <jiayuyeben@gmail.com>
2018-09-26 06:19:09 +0900

    first commit on this new branch

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,17 @@
 class MessagesController < ApplicationController
 
   def index
-    # @messages = Message.includes(:user).order("created_at DESC")
+    @messages = Message.includes(:user).order("created_at DESC")
   end
 
   def create
     Message.create(message_params)
   end
 
+private
+
+def message_params
+  params.require(:message).permit(:text, :image)
+end
 
 end
