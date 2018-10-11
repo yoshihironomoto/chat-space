a1af05a21647c3ba61cfb6d04400b1c87d02948e
野本佳裕 <jiayuyeben@gmail.com>
2018-10-11 12:35:50 +0900

    editing-some-files-for-inclimentalsearch

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -2,18 +2,28 @@ $(function() {
   function buildHTML(user) {
   	var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${user.name}</p>
-                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name= "${user.name}">追加</a>
                 </div>`
     $('#user-search-result').append(html);
   }
 
-  function buildNoHTNL(user) {
+  function buildNoHTML(user) {
   	var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${user}</p>
                 </div>`
     $('#user-search-result').append(html);
   }
 
+ 
+  function buildMemberHTML(id, name) {
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value= '${id}'>
+                  <p class='chat-group-user__name'>${name}</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
+    $('#chat-group-users').append(html);
+  }
+ 
 
   $('#user-search-field').on('keyup', function() {
     var input = $('#user-search-field').val();
@@ -23,9 +33,8 @@ $(function() {
       url: '/users',
       data: { keyword: input },
       dataType: 'json'
-
     })
-  
+
     .done(function(users) {
       $('#user-search-result').empty();
       if (users.length !== 0) {
@@ -35,12 +44,24 @@ $(function() {
       }
       else {
       	$('#user-search-result').empty();
-      	buildNoHTML("No user")
+      	buildNoHTML("No user");
       }
     })
-  
+
     .fail(function(){
       alert("失敗")
     })
   });
+
+  $(document).on("click", ".chat-group-user__btn--add", function () {
+    var id = $(this).attr('data-user-id');
+    var name = $(this).data('user-name');
+    buildMemberHTML(id, name); 
+    $(this).parent().remove();
+  })
+
+  $(document).on("click", ".js-remove-btn", function () {
+  	$(this).parent().remove();
+  })
+
 });
