0c422edeae104bcbe6a4322de42db0f8a78a21a6
野本佳裕 <jiayuyeben@gmail.com>
2018-10-11 14:56:33 +0900

    spacing

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,6 +1,7 @@
 $(function() {
   function buildHTML(user) {
-  	var html = `<div class="chat-group-user clearfix">
+  	var html = `
+				<div class="chat-group-user clearfix">
 					<p class="chat-group-user__name">${user.name}</p>
 					<a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name= "${user.name}">追加</a>
 				</div>`
